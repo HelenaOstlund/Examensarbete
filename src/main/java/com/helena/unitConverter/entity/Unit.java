49d191069d1053e11174aa18cd520097528eda68
HelenaOstlund <ostlund_helena@hotmail.com>
2023-01-20 15:08:27 +0100

    datbase added and UnitModel created

---
+++ src/main/java/com/helena/unitConverter/entity/Unit.java
@@ -0,0 +1,64 @@
+package com.helena.unitConverter.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "units")
+public class Unit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "unit_name")
+    private String unitName;
+
+    @Column(name = "unit_type")
+    private String unitType;
+
+    @Column(name = "conversion_rate")
+    private double conversionRate;
+
+    public Unit(){}
+
+    public Unit(Long id, String unitName, String unitType, double conversionRate) {
+        this.id = id;
+        this.unitName = unitName;
+        this.unitType = unitType;
+        this.conversionRate = conversionRate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getUnitType() {
+        return unitType;
+    }
+
+    public void setUnitType(String unitType) {
+        this.unitType = unitType;
+    }
+
+    public double getConversionRate() {
+        return conversionRate;
+    }
+
+    public void setConversionRate(double conversionRate) {
+        this.conversionRate = conversionRate;
+    }
+
+
+}
